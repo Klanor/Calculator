f695d57175087ef8c9fe3204a258be2dd9fe1bbc
Vladik <judosportage@gmail.com>
2020-02-16 13:02:33 +0200

    calculator project

---
+++ Header.h
@@ -0,0 +1,34 @@
+#ifndef CLAC_HEADER_H
+#define CLAC_HEADER_H
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+class Calculator
+{
+    std::vector<float>* _arguments;
+    std::vector<char>* _operators;
+    std::vector<float>* _queue;
+    float _result;
+
+    bool _FindOperator(std::string& buf, size_t& start);
+    void _ConvertToFloat(std::string& buf, size_t start, size_t end);
+    void _Calculate();
+
+public:
+    Calculator();
+
+    void Set(std::string buf);
+
+    std::string Get() const;
+
+    ~Calculator()
+    {
+        delete _arguments;
+        delete _operators;
+        delete _queue;
+    }
+};
+
+#endif //CLAC_HEADER_H
---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include "Header.h"
+
+using namespace std;
+
+int main()
+{
+    Calculator cl;
+    string h;
+
+    while (true)
+    {
+        cout << "___---===*SUPER CALCULATOR*===---___" << endl;
+        cout << "1 - to exit (without operation priority)" << endl;
+        cin >> h;
+        if (h == "1")
+            break;
+        try
+        {
+            cl.Set(h);
+        }
+        catch (string str)
+        {
+            cout << str << endl;
+            continue;
+        }
+        cout << cl.Get() << endl;
+        cout << endl << endl;
+    }
+
+
+    return 0;
+}
